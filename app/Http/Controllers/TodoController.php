75c02ef3dbcb3234c83cacf4ba1da334ff88101a
Lorenzo-Mariano <hhumminah@gmail.com>
2024-08-07 19:55:30 +0800

    Make todos finish-able

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -12,8 +12,32 @@ class TodoController extends Controller
     {
         return view('home', [
             'todos' => DB::table('todos')
-                ->select('id', 'title', 'description', 'is_done', 'finished_on')
+                ->orderBy('created_at', 'desc')
+                ->select('id', 'title', 'description', 'finished_on')
+                ->where('is_done', '=', 0)
                 ->paginate(5)
         ]);
+    }
+
+    public function showPageFinished()
+    {
+        return view(
+            'finished-todos',
+            [
+                'todos' => DB::table('todos')
+                    ->orderBy('created_at', 'desc')
+                    ->select('id', 'title', 'description', 'is_done', 'finished_on')
+                    ->where('is_done', '=', 1)
+                    ->paginate(5)
+            ]
+        );
+    }
+
+    public function finishTodo(string $id)
+    {
+        DB::table('todos')
+            ->where('id', '=', $id)
+            ->update(['is_done' => 1, 'finished_on' => date('Y-m-d H:i:s')]);
+        return redirect('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [TodoController::class, 'showPage']);
 
+Route::get('/finished-todos', [TodoController::class, 'showPage']);
+
 Route::get('/new-todo', function () {
     return view('new-todo');
 });
+
+Route::post('/todo/{id}', [TodoController::class, 'finishTodo']);
